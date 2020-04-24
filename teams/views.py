ef44f23ad03c8cfc8444e0562c2a3cffd2a26de3
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-24 17:47:07 +0100

    Create HomePageView

--- teams/views.py
+++ teams/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.views import View
+
+
+class HomePageView(View):
+    def get(self, request):
+        return HttpResponse("مرحبا بك في الصفحة الرئيسية")

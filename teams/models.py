f33e77bc127ed73ec51f1430aa59b34368e98a3f
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-24 19:39:49 +0100

    Create Models

--- teams/models.py
+++ teams/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    details = models.TextField()
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=256)
+    number = models.IntegerField()
+    age = models.IntegerField()
+    position_in_field = models.CharField(max_length=256,
+                                         choices=({'1': 'حارس', '2': 'مدافع', '3': 'وسط', '4': 'مهاجم'}))
+    is_captain = models.BooleanField(default=False)
+    team = models.ForeignKey(Team)
+
+
+class GameScore(models.Model):
+    first_team = models.CharField(max_length=256)
+    second_team = models.CharField(max_length=256)
+    first_team_score = models.IntegerField(default=0)
+    second_team_score = models.IntegerField(default=0)

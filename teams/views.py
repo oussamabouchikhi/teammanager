81018374bc19343617b7bc5611b024d77201fb0f
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-25 18:10:02 +0100

    Render teams using ListView class

--- teams/views.py
+++ teams/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.views import View
+from django.views.generic import ListView
 
 from teams.models import Team
 
-
+# Render teams using View class
 class HomePageView(View):
     def get(self, request):
         # get all teams from database
@@ -17,3 +18,11 @@ class HomePageView(View):
             "teams": all_teams
         }
         return render(request, 'teams_list.html', context)
+
+
+# Render teams using ListView class
+class TeamsListView(ListView):
+    model = Team
+    template_name = 'teams_list.html'
+    # Send all teams to template as teams
+    context_object_name = 'teams'

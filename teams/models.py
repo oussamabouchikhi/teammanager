62cb81ea0671033de520cd8cf6371189a0658d06
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-25 01:45:22 +0100

    Apply str method for all models

--- teams/models.py
+++ teams/models.py
@@ -6,6 +6,7 @@ class Team(models.Model):
     name = models.CharField(max_length=256, unique=True)
     details = models.TextField()
 
+    # Display Team name instead of Object when calling a Team instance from database
     def __str__(self):
         return self.name
 
@@ -19,9 +20,18 @@ class Player(models.Model):
     is_captain = models.BooleanField(default=False)
     team = models.ForeignKey(Team, on_delete=models.DO_NOTHING)
 
+    # Display playerName - clubName instead of Object
+    # example: بنزيما - ريال مدريد
+    def __str__(self):
+        return '{} - {}'.format(self.name, self.team)
+
 
 class GameScore(models.Model):
     first_team = models.CharField(max_length=256)
     second_team = models.CharField(max_length=256)
     first_team_score = models.IntegerField(default=0)
     second_team_score = models.IntegerField(default=0)
+
+    # example: ليفربول 0 - 3 ريال مدريد
+    def __str__(self):
+        return '{} {} - {} {}'.format(self.first_team, self.first_team_score, self.second_team_score, self.second_team)

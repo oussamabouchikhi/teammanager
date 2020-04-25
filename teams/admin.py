d8670b79f8fcea6e57430978c29f520fbafa87e0
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-25 01:22:48 +0100

    Register models

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+# to display tables in '/admin' we need to register models here
+from teams.models import Team, Player, GameScore
+
+admin.site.register(Team)
+admin.site.register(Player)
+admin.site.register(GameScore)

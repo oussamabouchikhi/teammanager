2f8cc9fb0c2ceab9ed64947c1f4c2540217a7f9d
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-29 23:48:12 +0100

    Install django-crispy-forms app

--- teams/forms.py
+++ teams/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.forms import ModelForm
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 from teams.models import Team
 
@@ -10,6 +12,11 @@ class TeamForm(forms.Form):
 
 
 class TeamModelForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(TeamModelForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper = layout.append(Submit('submit', 'اضافة'))
+
     class Meta:
         model = Team
         # fields = ['name', 'details']

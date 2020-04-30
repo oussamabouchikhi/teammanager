22cf46d4e385e5adc596501716603c7becaecbd4
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-04-30 18:39:38 +0100

    Create add_score form

--- teams/views.py
+++ teams/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.views.generic import ListView, DetailView
 
-from teams.forms import TeamForm, TeamModelForm
+from teams.forms import TeamForm, TeamModelForm, ScoreModelForm
 from teams.models import Team, GameScore, Player
 
 
@@ -35,6 +35,18 @@ class ScoresListView(ListView):
     model = GameScore
     template_name = 'scores_list.html'
     context_object_name = 'scores'
+
+    def get_context_data(self, **kwargs):
+        context = super(ScoresListView, self).get_context_data(**kwargs)
+        context['form'] = ScoreModelForm()
+        return context
+
+    # Create a new game-score from form
+    def post(self, request, *args, **kwargs):
+        form = ScoreModelForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/scores/')
 
 
 class TeamDetailsView(DetailView):

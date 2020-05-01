ba1816369173bf6bb849ec8c22b098eb06c18612
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-05-01 18:44:54 +0100

    Add team creation test

--- teams/tests.py
+++ teams/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from teams.models import Team
+
+
+class ModelTestCase(TestCase):
+    # Test if a team creation is running well
+    def test_team_creation(self):
+        # Create a new team
+        team = Team.objects.create(name='فريقي', details="فريق الاختبار")
+        # Check if team name is equal to the given name
+        self.assertEqual(team.name, 'فريقي')
